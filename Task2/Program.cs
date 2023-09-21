abd4273346867cd53a15c082659bc30602004e06
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-22 00:17:52 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2-b1)/(k2-k1);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Две прямые пересекаются в точке с координатами ({x},{y})");
