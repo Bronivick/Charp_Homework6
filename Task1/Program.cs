c406cd61f66fd110ae94b6622542d137ef5a1ced
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-22 00:17:15 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите кол-во элементов массива:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[num];
+
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine($"Введите {i+1} элемент массива: ");
+    array[i]= Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Кол-во чисел больше 0 =" + count);
